1cd24b9eb9ddfeec5a2ac14b1daf3014ff262c15
C12H22O12 <director20844@gmail.com>
2023-04-23 23:37:00 +0900

    fix: add Input case in buffer

--- Etc/Input/InputWithBuffer.java
+++ Etc/Input/InputWithBuffer.java
@@ -9,11 +9,21 @@ public class Main {
         br.close();
     }
 
-    //문자열 분리
-    public static void splitString() throws IOException {
+    //split 메서드를 이용하여 문자열 분리
+    public static void splitString1() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader((System.in)));
         String[] str = br.readLine().split(" ");
 
         for (String s: str) System.out.println((Integer.parseInt(s)));
+    }
+
+    //StringTokenizer을 이용하여 문자열 분리
+    public static void splitString2() throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine(), " ");
+        int M = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+
+        bf.close();
     }
 }
